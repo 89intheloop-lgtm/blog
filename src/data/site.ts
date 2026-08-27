d27e3a781c34cc39f28e0cd867e0f9964158c18a
saakshidas <sakshidas83498@gmail.com>
2026-08-27 13:19:50 +0530

    added googlesiteverificaton

--- src/data/site.ts
+++ src/data/site.ts
@@ -10,7 +10,7 @@ export const siteConfig = {
   language: 'en',
   twitterHandle: '@saakshidas99',
   defaultOgImage: '/images/og-default.png',
-  googleSiteVerification: '',
+  googleSiteVerification: 'googleacf2939e25427803.html', 
   social: {
     github: 'https://github.com/89intheloop-lgtm',
     freelancer: 'https://www.freelancer.com/u/sakshismm',
